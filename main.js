07358f6965982eeca6216a74786fadbccd93f83e
sid-max1996 <sid-max1996@yandex.ru>
2019-11-07 14:19:46 +0300

    first commit

--- main.js
+++ main.js
@@ -1,53 +1,73 @@
-// Modules to control application life and create native browser window
-const {app, BrowserWindow} = require('electron')
+const { app, BrowserWindow, ipcMain } = require('electron')
 const path = require('path')
+const ip = require('ip')
 
-// Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the JavaScript object is garbage collected.
+const express = require('express')
+const bodyParser = require('body-parser')
+const expressApp = express()
+const cors = require('cors')
+const server = require('http').createServer(expressApp)
+
+expressApp.use(cors())
+
+expressApp.use(bodyParser.urlencoded({ extended: false }))
+expressApp.use(bodyParser.json())
+expressApp.use(bodyParser.raw())
+expressApp.use(bodyParser.text())
+
 let mainWindow
 
 function createWindow () {
-  // Create the browser window.
   mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
+    autoHideMenuBar: true,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js')
     }
   })
 
-  // and load the index.html of the app.
   mainWindow.loadFile('index.html')
 
-  // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
-
-  // Emitted when the window is closed.
   mainWindow.on('closed', function () {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
     mainWindow = null
   })
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
 app.on('ready', createWindow)
 
-// Quit when all windows are closed.
 app.on('window-all-closed', function () {
-  // On macOS it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
   if (process.platform !== 'darwin') app.quit()
 })
 
 app.on('activate', function () {
-  // On macOS it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
   if (mainWindow === null) createWindow()
 })
 
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
+expressApp.use(function (req, res, next) {
+  console.log('req')
+  const { method, body, query, path, headers } = req
+  const params = { method, body, query, path, headers }
+  console.log('params', params)
+  mainWindow.webContents.send('server-query', params)
+  res.sendStatus(200)
+})
+
+function runServer (port) {
+  return new Promise((resolve) => {
+    server.listen(port, err => {
+      if (err) resolve({ isSuccess: false })
+      else {
+        resolve({
+          isSuccess: true,
+          serverAddr: `http://${ip.address()}:${port}`
+        })
+      }
+    })
+  })
+}
+
+ipcMain.handle('run-server', async (event, port) => {
+  const result = await runServer(port)
+  return result
+})
--- preload.js
+++ preload.js
@@ -1,12 +1,33 @@
-// All of the Node.js APIs are available in the preload process.
-// It has the same sandbox as a Chrome extension.
+const { ipcRenderer } = require('electron')
+
+ipcRenderer.on('server-query', (e, params) => {
+  console.log('server-query params', params)
+  const { method, body, query, path, headers } = params
+  const queryMessageElem = document.getElementById('query-message')
+  queryMessageElem.style.visibility = 'visible'
+  document.getElementById('method').innerHTML = `МЕТОД: ${method}`
+  document.getElementById('path').innerHTML = `ПУТЬ: ${path}`
+  document.getElementById('query').innerHTML = `ПАРАМЕТРЫ: ${JSON.stringify(query)}`
+  document.getElementById('body').innerHTML = `ТЕЛО: ${JSON.stringify(body)}`
+  document.getElementById('headers').innerHTML = `ЗАГОЛОВКИ: ${JSON.stringify(headers)}`
+})
+
 window.addEventListener('DOMContentLoaded', () => {
-  const replaceText = (selector, text) => {
-    const element = document.getElementById(selector)
-    if (element) element.innerText = text
-  } 
-  
-  for (const type of ['chrome', 'node', 'electron']) {
-    replaceText(`${type}-version`, process.versions[type])
+  const runBtn = document.getElementById('run-btn')
+  runBtn.onclick = (e) => {
+    const messageElem = document.getElementById('message')
+    const portValue = document.getElementById('port-input').value
+    ipcRenderer.invoke('run-server', portValue).then((res) => {
+      if (res.isSuccess) {
+        runBtn.disabled = true
+        messageElem.innerHTML = `Cервер запущен по адресу: ${res.serverAddr}`
+        messageElem.style.visibility = 'visible'
+        messageElem.classList.add('alert-secondary')
+      } else {
+        messageElem.innerHTML = 'Ошибка запуска сервера'
+        messageElem.style.visibility = 'visible'
+        messageElem.classList.add('alert-warning')
+      }
+    })
   }
 })
